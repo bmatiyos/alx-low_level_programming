c832d1cf2d2c39f22471f57d1876a043d8b26c88
bmatiyos <bmatiyos@gmail.com>
2022-12-12 03:13:09 -0800

    ptint alpabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char c;
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putcher('\n');
+	return(0);
+}
+
