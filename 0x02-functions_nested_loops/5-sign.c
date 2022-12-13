7c1f9aab66fe825bcfc85eeca139db2aad02da22
bmatiyos <bmatiyos@gmail.com>
2022-12-13 03:27:25 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - print sign of the number
+ * @n: number of the sign
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		_putchar('0');
+		return (0);
+}
