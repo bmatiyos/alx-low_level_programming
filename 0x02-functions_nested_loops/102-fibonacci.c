8f42125934f4c4ad52256486c48f4847ddf4a45b
bmatiyos <bmatiyos@gmail.com>
2022-12-13 09:16:21 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - print tht first 50 num
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	long int i, j, k, next;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+
+	{
+
+		if (j != 20365011074)
+
+		{
+
+			printf("%ld, ", j);
+
+		}
+
+		else
+
+		{
+
+			printf("%ld\n", j);
+
+		}
+
+		next = j + k;
+
+		j = k;
+
+		k = next;
+
+	}
+
+	return (0);
+
+}
