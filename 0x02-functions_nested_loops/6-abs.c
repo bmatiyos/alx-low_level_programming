080cbbc1e72433b8629e1828dbeb2bbcdcf2f176
bmatiyos <bmatiyos@gmail.com>
2022-12-13 03:42:07 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compits the obsolute of an integer
+ * @n: integer for which to grt an absolute value
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
