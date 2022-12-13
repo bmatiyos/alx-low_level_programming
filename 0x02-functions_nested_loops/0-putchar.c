3389dab50124c02e9f43922e21d4a462afa2a11c
bmatiyos <bmatiyos@gmail.com>
2022-12-13 02:09:39 -0800

    functions loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - ptint putchar
+ * main - print putchar
  *
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
