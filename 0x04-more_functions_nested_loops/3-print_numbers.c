1ac5fb27f672af8d443be3acabf6aebdeb5acaac
bmatiyos <bmatiyos@gmail.com>
2022-12-15 04:14:02 -0800

    num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,13 +7,11 @@
  */
 void print_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i > 10)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i)
-		_putchar('\n')
-		i++;
+		_putchar(i);
 	}
-return (0);
+	_putchar('\n');
 }
