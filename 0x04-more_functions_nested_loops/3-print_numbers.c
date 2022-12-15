0b26e2931f5a6cf252fc0716dccfe88b729e41f3
bmatiyos <bmatiyos@gmail.com>
2022-12-15 03:36:38 -0800

    print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i > 10)
+	{
+		_putchar(i)
+		_putchar('\n')
+		i++;
+	}
+return (0);
+}
