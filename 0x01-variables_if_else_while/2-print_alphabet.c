7eb97908d486c95ec26234d72a41f8b9a8d204bc
bmatiyos <bmatiyos@gmail.com>
2022-12-15 22:11:40 -0800

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-#includ <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
